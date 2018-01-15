499683eab84d04b209b3c3965797bfbf340f5bca
John Mollman <me@johnmollman.com>
2018-01-14 20:52:12 -0500

    Set lowercase URLs for routing

--- HackerNewsClone/App_Start/RouteConfig.cs
+++ HackerNewsClone/App_Start/RouteConfig.cs
@@ -9,6 +9,7 @@ namespace HackerNewsClone {
     public class RouteConfig {
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.LowercaseUrls = true;
 
             routes.MapRoute(
                 name: "Default",
