ef7d96cbc4414448f552407983a449ba03234680
John Mollman <me@johnmollman.com>
2018-03-05 22:01:59 -0500

    Move Owin configuration into Startup.Auth.cs

---
+++ Hyve/App_Start/Startup.Auth.cs
@@ -0,0 +1,22 @@
+﻿using Hyve.App_Start;
+using Hyve.Models.Contexts;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace Hyve {
+    public partial class Startup {
+        public void ConfigureAuth(IAppBuilder app) {
+            app.CreatePerOwinContext(HyveDbContext.Create);
+            app.CreatePerOwinContext<UserManager>(UserManager.Create);
+            app.CreatePerOwinContext<SignInManager>(SignInManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions() {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/login"),
+                ReturnUrlParameter = "redirect",
+            });
+        }
+    }
+}
--- Hyve/Startup.cs
+++ Hyve/Startup.cs
@@ -1,22 +1,12 @@
-﻿using Hyve.App_Start;
-using Hyve.Models.Contexts;
-using Microsoft.AspNet.Identity;
-using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
+﻿using Microsoft.Owin;
 using Owin;
 
-namespace Hyve {
-    public class Startup {
-        public void Configuration(IAppBuilder app) {
-            app.CreatePerOwinContext(HyveDbContext.Create);
-            app.CreatePerOwinContext<UserManager>(UserManager.Create);
-            app.CreatePerOwinContext<SignInManager>(SignInManager.Create);
+[assembly: OwinStartupAttribute(typeof(Hyve.Startup))]
 
-            app.UseCookieAuthentication(new CookieAuthenticationOptions() {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/login"),
-                ReturnUrlParameter = "redirect",
-            });
+namespace Hyve {
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
         }
     }
 }
