125505e0acb2b290b8179e113ccdf46eb3997079
John Mollman <me@johnmollman.com>
2018-06-14 00:20:44 -0400

    Add more functionality to the gulpfile

--- Hyve/gulpfile.js
+++ Hyve/gulpfile.js
@@ -4,57 +4,58 @@ var sass = require("gulp-sass");
 var concat = require("gulp-concat");
 var cleanCss = require("gulp-clean-css");
 var uglify = require("gulp-uglify");
+var babel = require("gulp-babel");
 var imagemin = require("gulp-imagemin");
 var cache = require("gulp-cache");
+var autoprefixer = require("gulp-autoprefixer");
+var del = require("del");
+var runSequence = require("run-sequence");
 var browserSync = require("browser-sync").create();
 
-// Compile and concatenate all SCSS files
 gulp.task("sass", function () {
     return gulp.src(config.srcPath + "/scss/**/*.scss")
         .pipe(sass())
+        .pipe(autoprefixer())
         .pipe(concat(config.concattedCssFilename))
         .pipe(gulp.dest(config.destPath + "/css/"))
         .pipe(browserSync.reload({ stream: true }));
 });
 
-// Compile, concatenate, and minify all SCSS files
-gulp.task("sass-minify", function () {
+gulp.task("sass-min", function () {
     return gulp.src(config.srcPath + "/scss/**/*.scss")
         .pipe(sass())
+        .pipe(autoprefixer())
         .pipe(concat(config.concattedMinifiedCssFilename))
         .pipe(cleanCss())
         .pipe(gulp.dest(config.destPath + "/css/"));
 });
 
-// Concatenate all JS files
 gulp.task("js", function () {
     return gulp.src(config.srcPath + "/js/**/*.js")
+        .pipe(babel())
         .pipe(concat(config.concattedJsFilename))
         .pipe(gulp.dest(config.destPath + "/js/"));
 });
 
-// Concatenate and minify all JS files
-gulp.task("js-minify", function () {
+gulp.task("js-min", function () {
     return gulp.src(config.srcPath + "/js/**/*.js")
+        .pipe(babel())
         .pipe(concat(config.concattedMinifiedJsFilename))
         .pipe(uglify())
         .pipe(gulp.dest(config.destPath + "/js/"));
 });
 
-// Optimize images
 gulp.task("images", function () {
     return gulp.src(config.srcPath + "/img/**/*.+(png|jpg|jpeg|gif|svg)")
         .pipe(cache(imagemin()))
         .pipe(gulp.dest(config.destPath + "/img/"));
 });
 
-// Move fonts
 gulp.task("fonts", function () {
     return gulp.src(config.srcPath + "/fonts/**/*")
         .pipe(gulp.dest(config.destPath + "/fonts/"));
 });
 
-// Setup BrowserSync
 gulp.task("sync", function () {
     browserSync.init({
         proxy: config.browserSyncSettings.proxy,
@@ -62,15 +63,20 @@ gulp.task("sync", function () {
     });
 });
 
-// Watch for file changes
-gulp.task("watch", ["sass", "js", "fonts", "sync"], function () {
+gulp.task("watch", ["clean", "sass", "js", "fonts", "sync"], function () {
     gulp.watch(config.watchPaths.scss, ["sass"]);
     gulp.watch(config.watchPaths.js, browserSync.reload);
     gulp.watch(config.watchPaths.cshtml, browserSync.reload);
 });
 
-// Concatenate + minify everything (ready for release)
-gulp.task("release", ["sass-minify", "js-minify", "images", "fonts"]);
+gulp.task("clean", function () {
+    return del.sync(config.destPath);
+});
 
-// Default
-gulp.task("default", ["watch"])
+//gulp.task("build", ["clean", "sass-min", "js-min", "images", "fonts"]);
+gulp.task("build", function (cb) {
+    // Make sure 'clean' runs first, then all the others
+    runSequence("clean", ["sass-min", "js-min", "images", "fonts"], cb);
+});
+
+gulp.task("default", ["watch"]);
