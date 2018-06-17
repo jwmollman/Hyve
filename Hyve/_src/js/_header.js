ec714999ebdf3885f8fc6ff0cf4159172068051f
John Mollman <me@johnmollman.com>
2018-06-17 02:50:05 -0400

    Cleanup

--- Hyve/_src/js/_header.js
+++ Hyve/_src/js/_header.js
@@ -1,8 +1,11 @@
-﻿document.getElementById("username").addEventListener("click", () => {
-    const authNav = document.getElementById("auth-nav");
-    if (authNav.style.display === "block") {
-        document.getElementById("auth-nav").style.display = "none";
-    } else {
-        document.getElementById("auth-nav").style.display = "block";
-    }
-});
+﻿const usernameDropdownField = document.getElementById("username");
+if (usernameDropdownField) {
+    usernameDropdownField.addEventListener("click", () => {
+        const authNav = document.getElementById("auth-nav");
+        if (authNav.style.display === "block") {
+            document.getElementById("auth-nav").style.display = "none";
+        } else {
+            document.getElementById("auth-nav").style.display = "block";
+        }
+    });
+}
