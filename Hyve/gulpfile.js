4887c8f0e349d0adee45030e1dd7a1066a64fc12
John Mollman <me@johnmollman.com>
2018-06-17 21:36:12 -0400

    Modify gulpfile to use ES6 features, and add a watcher for *.html files

--- Hyve/gulpfile.js
+++ Hyve/gulpfile.js
@@ -1,19 +1,37 @@
-var config = require("./gulpfile-config");
-var gulp = require("gulp");
-var watch = require("gulp-watch");
-var sass = require("gulp-sass");
-var concat = require("gulp-concat");
-var cleanCss = require("gulp-clean-css");
-var uglify = require("gulp-uglify-es").default;
-var imagemin = require("gulp-imagemin");
-var cache = require("gulp-cache");
-var rev = require("gulp-rev");
-var autoprefixer = require("gulp-autoprefixer");
-var del = require("del");
-var runSequence = require("run-sequence");
-var browserSync = require("browser-sync").create();
+const gulp = require("gulp");
+const watch = require("gulp-watch");
+const sass = require("gulp-sass");
+const concat = require("gulp-concat");
+const cleanCss = require("gulp-clean-css");
+const uglify = require("gulp-uglify-es").default;
+const imagemin = require("gulp-imagemin");
+const cache = require("gulp-cache");
+const rev = require("gulp-rev");
+const autoprefixer = require("gulp-autoprefixer");
+const del = require("del");
+const runSequence = require("run-sequence");
+const browserSync = require("browser-sync").create();
 
-gulp.task("sass", function () {
+const config = {
+    srcPath: "_src",
+    destPath: "Content",
+    concattedCssFilename: "main.css",
+    concattedJsFilename: "main.js",
+    concattedMinifiedCssFilename: "main.min.css",
+    concattedMinifiedJsFilename: "main.min.js",
+    watchPaths: {
+        scss: "_src/scss/**/*.scss",
+        js: "_src/js/**/*.js",
+        html: "./**/*.html",
+        cshtml: "Views/**/*.cshtml",
+    },
+    browserSyncSettings: {
+        proxy: "http://localhost:12259/",
+        host: "localhost",
+    },
+};
+
+gulp.task("sass", () => {
     return gulp.src(config.srcPath + "/scss/main.scss")
         .pipe(sass())
         .on("error", onError)
@@ -23,7 +41,7 @@ gulp.task("sass", function () {
         .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task("sass-min", function () {
+gulp.task("sass-min", () => {
     return gulp.src(config.srcPath + "/scss/main.scss")
         .pipe(sass())
         .pipe(autoprefixer())
@@ -38,14 +56,14 @@ gulp.task("sass-min", function () {
         .pipe(gulp.dest(config.destPath));
 });
 
-gulp.task("js", function () {
+gulp.task("js", () => {
     return gulp.src(config.srcPath + "/js/**/*.js")
         .pipe(concat(config.concattedJsFilename))
         .on("error", onError)
         .pipe(gulp.dest(config.destPath + "/js/"));
 });
 
-gulp.task("js-min", function () {
+gulp.task("js-min", () => {
     return gulp.src(config.srcPath + "/js/**/*.js")
         .pipe(concat(config.concattedMinifiedJsFilename))
         .pipe(uglify())
@@ -58,48 +76,51 @@ gulp.task("js-min", function () {
         .pipe(gulp.dest(config.destPath));
 });
 
-gulp.task("images", function () {
+gulp.task("images", () => {
     return gulp.src(config.srcPath + "/img/**/*.+(png|jpg|jpeg|gif|svg)")
         .pipe(cache(imagemin()))
         .pipe(gulp.dest(config.destPath + "/img/"));
 });
 
-gulp.task("fonts", function () {
+gulp.task("fonts", () => {
     return gulp.src(config.srcPath + "/fonts/**/*")
         .pipe(gulp.dest(config.destPath + "/fonts/"));
 });
 
-gulp.task("sync", function () {
+gulp.task("sync", () => {
     browserSync.init({
         proxy: config.browserSyncSettings.proxy,
         host: config.browserSyncSettings.host
     });
 });
 
-gulp.task("watch", ["sass", "js", "images", "fonts", "sync"], function () {
+gulp.task("watch", ["sass", "js", "images", "fonts", "sync"], () => {
     //gulp.watch(config.watchPaths.scss, ["sass"]);
     //gulp.watch(config.watchPaths.js, browserSync.reload);
     //gulp.watch(config.watchPaths.cshtml, browserSync.reload);
 
     // Because gulp.watch() can't watch for new/deleted files, use gulp-watch instead
-    watch(config.watchPaths.scss, function () {
+    watch(config.watchPaths.scss, () => {
         gulp.start("sass");
     });
-    watch(config.watchPaths.js, function () {
+    watch(config.watchPaths.js, () => {
         gulp.start("js");
         browserSync.reload();
     });
-    watch(config.watchPaths.cshtml, function () {
+    watch(config.watchPaths.html, () => {
+        browserSync.reload();
+    });
+    watch(config.watchPaths.cshtml, () => {
         browserSync.reload();
     });
 });
 
-gulp.task("clean", function () {
+gulp.task("clean", () => {
     return del.sync(config.destPath);
 });
 
 //gulp.task("build", ["clean", "sass-min", "js-min", "images", "fonts"]);
-gulp.task("build", function (cb) {
+gulp.task("build", cb => {
     // Make sure 'clean' runs first, then all the others
     runSequence("clean", "sass-min", "js-min", "images", "fonts", cb);
 });
